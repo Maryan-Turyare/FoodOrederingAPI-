9bbee12298f7bd590bad303b4b5ec402542993dc
Maryan-Turyare <qaalimaxamed850@gmail.com>
2026-05-20 03:53:03 -0700

    Added README and Postman collection

---
+++ README.md
@@ -0,0 +1,29 @@
+# FoodOrederingAPI
+
+## Project Description
+This is a Food Ordering System built using ASP.NET Core Web API and PostgreSQL.
+
+## Features
+- User Registration
+- User Login with JWT Authentication
+- Get All Foods
+- Add New Food
+- Unit Testing using xUnit
+
+## Technologies Used
+- ASP.NET Core
+- PostgreSQL
+- Entity Framework Core
+- JWT Authentication
+- xUnit
+- Moq
+
+## How to Run
+1. dotnet restore
+2. dotnet run
+
+## Run Tests
+dotnet test
+
+## Author
+Maryan Turyare
--- obj/Debug/net10.0/FoodOrederingAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/FoodOrederingAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodOrederingAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d35462edb9630edce5f30cbd83580bfc0643c205")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodOrederingAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodOrederingAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
