a4aec0977345806cc939b2b683c90109ed2fc80e
Maryan-Turyare <qaalimaxamed850@gmail.com>
2026-06-02 02:38:52 -0700

    Added Dockerfile for Render deployment

--- obj/Debug/net10.0/FoodOrederingAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/FoodOrederingAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodOrederingAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36d8226f3066fb47f0a26cffbf0b0a7ab22e2303")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b35175db31711cbc118baf58d5a3291acdc1d5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodOrederingAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodOrederingAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
